96b5e9ea1e65ebc2581f06c00c7898715f0d88d3
lurbaby <olexandrchoriy@gmail.com>
2024-04-01 02:26:35 +0300

    added dockerfile

--- architecture/ec2_module/main.tf
+++ architecture/ec2_module/main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "microserv-exmpl" {
+resource "aws_instance" "protectOne" {
 	
 	ami = var.ami
 	instance_type = var.instance_type
--- architecture/sg_module/output.tf
+++ architecture/sg_module/output.tf
@@ -1,3 +1,3 @@
 output "security_group_id" {
-	value = aws_security_group.protecOne-sg.id
+	value = aws_security_group.protectOne-sg.id
 }
